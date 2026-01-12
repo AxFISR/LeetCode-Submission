296f49f744cb72341a34ab3a659c8e228b0f31ba
Alex Baboshin <105110179+AxFISR@users.noreply.github.com>
2026-01-12 12:19:29 +0200

    Time: 206 ms (5.05%) | Memory: 16.3 MB (5.21%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,12 +1,28 @@
 class Solution(object):
     def isPalindrome(self, s):
-        s = s.lower()
-        s = "".join(ch for ch in s if ch.isalnum())
+        """
+        :type s: str
+        :rtype: bool
+        """
+        skipIndex = 0
+        newstr = ""
+        #gonna rewrite the enire string, keeping only the letters
 
-        left, right = 0, len(s) - 1
-        while left < right:
-            if s[left] != s[right]:
+        for skipIndex in range(len(s)):
+            if ord(s[skipIndex]) >= 65 and ord(s[skipIndex]) <= 90:
+                newstr += chr(ord(s[skipIndex]) + 32)
+            elif ord(s[skipIndex]) >= 97 and ord(s[skipIndex]) <= 122:
+                newstr += s[skipIndex]
+            elif ord(s[skipIndex]) >= 48 and ord(s[skipIndex]) <= 57:
+                newstr += s[skipIndex]
+            else:
+                continue
+
+        l=0
+        r=len(newstr)-1
+        while(l<r):
+            if(newstr[l] != newstr[r]):
                 return False
-            left += 1
-            right -= 1
+            l+=1
+            r-=1
         return True
